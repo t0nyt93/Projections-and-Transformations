bcdbe3a66bcd41f9068347da418b2efbcc7e2545
Tony <tonytyrrell93@gmail.com>
2016-04-30 11:37:59 -0700

    First Commit

---
+++ mat4.cpp
@@ -0,0 +1,194 @@
+#include "vec3.h"
+#include "mat4.h"
+
+	// ** 2A: **
+
+Mat4::Mat4( )
+{
+	SetIdentity( );
+}
+
+
+Mat4::Mat4(	float a, float b, float c, float d, float e, float f, float g, float h,
+		float i, float j, float k, float l, float n, float o, float p, float q )
+{
+	m[0][0]	= a;	m[0][1]	= b;	m[0][2]	= c;	m[0][3]	= d;
+	m[1][0]	= e;	m[1][1]	= f;	m[1][2]	= g;	m[1][3]	= h;
+	m[2][0]	= i;	m[2][1]	= j;	m[2][2]	= k;	m[2][3]	= l;
+	m[3][0]	= n;	m[3][1]	= o;	m[3][2]	= p;	m[3][3]	= q;
+}
+
+Mat4&
+Mat4::operator=( const Mat4& that )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+	// "that" is a reference to the other matrix (that.name)
+	int i,j,k;
+	for(i=0; i < 4; i++ ){
+		for(j=0;j<4;j++)
+			{
+			this->m[i][j] = that.m[i][j];
+			}
+	}
+	
+	return *this;
+}
+
+Mat4
+Mat4::operator*( Mat4& that )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+	// "that" is a reference to the other matrix (that.name)
+    int i,j,k;
+	Mat4 result;
+	for(i=0;i<4;i++)
+	{	
+		for (int j = 0; j < 4; ++j)
+		{
+			result.m[i][j] = 0;
+			for (int k = 0; k < 4; ++k)
+			{	
+				result.m[i][j] += this->m[i][k] * that.m[k][j];
+			}
+			/* code */
+		}
+	}
+	//result.m ???
+	return result;
+}
+
+Vec3
+Mat4::operator*( Vec3& that )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+	// "that" is a reference to the vector (that.name)
+
+	Vec3 result;
+	result.x = this->m[0][0] * that.x + this->m[0][1] * that.y + this->m[0][2] * that.z + this->m[0][3];
+	result.y = this->m[1][0] * that.x + this->m[1][1] * that.y + this->m[1][2] * that.z + this->m[1][3];
+	result.z = this->m[2][0] * that.x + this->m[2][1] * that.y + this->m[2][2] * that.z + this->m[2][3];
+	
+	return result;;
+}
+
+void
+Mat4::Print( char *str, FILE *fp )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+
+	fprintf( fp, "%s \n", str );
+	fprintf( fp, "\t%8.2f  %8.2f  %8.2f  %8.2f\n", this->m[0][0], this->m[0][1], this->m[0][2], this->m[0][3] );
+	fprintf( fp, "\t%8.2f  %8.2f  %8.2f  %8.2f\n", this->m[1][0], this->m[1][1], this->m[1][2], this->m[1][3] );
+	fprintf( fp, "\t%8.2f  %8.2f  %8.2f  %8.2f\n", this->m[2][0], this->m[2][1], this->m[2][2], this->m[2][3] );
+	fprintf( fp, "\t%8.2f  %8.2f  %8.2f  %8.2f\n", this->m[3][0], this->m[3][1], this->m[3][2], this->m[3][3] );
+}
+
+void
+Mat4::SetIdentity( )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+	int x,y;
+	for(x=0;x<4;x++)
+	{
+		for(y=0;y<4;y++)
+		{
+			if(x == y)
+			{
+				this->m[x][y] = 1;
+			}
+			else
+				this->m[x][y] = 0;
+
+		}
+
+	}
+
+}
+
+	// ** 2B: **
+
+void
+Mat4::SetRotateZ( float deg )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+	this->SetIdentity();
+	float rad = D2R * deg;
+	m[0][0] = m[1][1] = cos(rad);
+	m[0][1] = -sin(rad);
+	m[1][0] = sin(rad);
+}
+
+void
+Mat4::SetRotateY( float deg )
+{
+	this->SetIdentity();
+	float rad = D2R * deg;
+	m[0][0] = m[2][2] = cos(rad);
+	m[0][2] = sin(rad);
+	m[2][0] = -sin(rad);
+
+}
+
+void
+Mat4::SetRotateX( float deg )
+{
+
+	this->SetIdentity();
+	float rad = D2R * deg;
+	m[1][1] = m[2][2] = cos(rad);
+	m[1][2] = -sin(rad);
+	m[2][1] = sin(rad);
+
+}
+
+
+
+void
+Mat4::SetScale( float sx, float sy, float sz )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+	m[0][0] *= sx;
+	m[1][1] *= sy;
+	m[2][2] *= sz;
+	//this->m ???
+}
+void
+Mat4::SetOrthoGraphic(float l,float r, float t, float b, float n, float f)
+{
+	m[0][0] = 1. / r;	m[0][1] = 0.;	m[0][2] = 0.;	m[0][3] = 0.;//-((r + l) / (r - l));
+	m[1][0] = 0.;	m[1][1] = 1. / t;	m[1][2] = 0.;	m[1][3] = 0.;// -((t + b) / (t - b));
+	m[2][0] = 0.;	m[2][1] = 0.;	m[2][2] = -2/(f-n);	m[2][3] = -((f+n)/(f-n));
+	m[3][0] = 0.;	m[3][1] = 0.;	m[3][2] = 0.;	m[3][3] = 1.;
+
+}
+void
+Mat4::SetPerspective(double a, double nZ, double fZ)
+{
+	a *= D2R;
+	m[0][0] = 1. /tan(a/2.) ;	m[0][1] = 0.;	m[0][2] = 0.;	m[0][3] = 0.;
+	m[1][0] = 0.;	m[1][1] = 1/tan(a/2.);	m[1][2] = 0.;	m[1][3] = 0.;
+	m[2][0] = 0.;	m[2][1] = 0.;	m[2][2] = (-nZ-fZ)/nZ-fZ;	m[2][3] = (2. * nZ * fZ)/(nZ - fZ);
+	m[3][0] = 0.;	m[3][1] = 0.;	m[3][2] = 1.;	m[3][3] = 0.;
+
+}
+
+void
+Mat4::SetTranslate( float tx, float ty, float tz )
+{
+	// "this" is a pointer to this class's mat4  (this->name)
+
+	m[0][3] = tx;
+	m[1][3] = ty;
+	m[2][3] = tz;
+
+}
+void
+Mat4::SetCamera(Vec3 U)
+{
+
+	m[0][0] = U.x;	m[0][1] = 0.;	m[0][2] = 0.;	m[0][3] = 0.;
+	m[1][0] = 0.;	m[1][1] = U.y;	m[1][2] = 0.;	m[1][3] = 0.;
+	m[2][0] = 0.;	m[2][1] = 0.;	m[2][2] = U.z;	m[2][3] = 0.;
+	m[3][0] = 0.;	m[3][1] = 0.;	m[3][2] = 0.;	m[3][3] = 1.;
+
+}
---
+++ matlib.cpp
@@ -0,0 +1,373 @@
+/*
+
+Dummy routines for matrix transformations.
+
+These are for you to write!
+
+*/
+
+
+#include <stdio.h>
+#include <math.h>
+#include <fstream>
+#include <Windows.h>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <queue>
+#include "osuGraphics.h"
+#include "lines.h"
+#include "vec3.h"
+#include "mat4.h"
+//-------------------------------------------------
+
+//------Data Structures ------------
+struct myVertex {
+	double myX = 0, myY = 0;
+	int red, green, blue;
+	int initialized = 0;
+};
+struct my3DVertex {
+	Vec3 pos = Vec3();
+	Vec3 color = Vec3();
+	int intialized;
+};
+
+struct myTriangle {
+	myVertex one = {}, two = {}, three = {};
+	int currRed, currGreen, currBlue;
+	double minX, maxX, minY, maxY;
+}tDraw;
+
+struct myLine {
+	my3DVertex one, two;
+}lDraw;
+
+BOOL ORTHO = FALSE;
+BOOL PROJECTION = FALSE;
+
+//-------------------
+std::queue<Mat4> matStack;
+Vec3 u;
+Mat4 Current_Transform = Mat4();
+Mat4 persProj = Mat4();
+Mat4 orthoProj = Mat4();
+Mat4 myView = Mat4();
+//-------------------
+void osuOrtho(double left, double right, double bottom, double top, double nearp,double farp)
+{ 
+	
+	orthoProj.SetOrthoGraphic(left, right, top, bottom, nearp, farp);
+	printf("Set Ortho graphic\n");
+	orthoProj.Print();
+	ORTHO = TRUE;
+	PROJECTION = FALSE;
+}
+void osuPerspective(double fovy, double nearp, double farp) 
+{
+	persProj.SetPerspective(fovy, nearp, farp);
+	PROJECTION = TRUE;
+	ORTHO = FALSE;
+}
+
+//Specify the RGB of all subsequent vertices that will be drawn
+void osuColor3f(double red, double green, double blue)
+{
+	//Convert float numbers to a r-g-b that can be written to a pixel
+	tDraw.currRed = red * 255;
+	tDraw.currGreen = green * 255;
+	tDraw.currBlue = blue * 255;
+
+}
+
+//Specify x/y position in screen space of a vertex. Add it to image->values
+void osuVertex2f(double x, double y)
+{
+
+	int w, h;
+	osuGetFramebufferSize(&w, &h);
+	//convert the 0 -> 1 coordinates into pixel coordinates.
+	x *= w;
+	y *= h;
+
+	//If we haven't initialized our first vertex yet...do so
+	if (tDraw.one.initialized == 0)
+	{
+		tDraw.one.initialized = 1;
+		tDraw.one.myX = x;
+		tDraw.minX = x;
+		tDraw.maxX = x;
+		tDraw.one.myY = y;
+		tDraw.minY = y;
+		tDraw.maxY = y;
+		tDraw.one.red = tDraw.currRed;
+		tDraw.one.green = tDraw.currGreen;
+		tDraw.one.blue = tDraw.currBlue;
+	}
+	//If we haven't initialized our second vertex yet...do so
+
+	else if (tDraw.two.initialized == 0)
+	{
+
+		tDraw.two.initialized = 1;
+		tDraw.two.myX = x;
+		tDraw.two.myY = y;
+		tDraw.two.red = tDraw.currRed;
+		tDraw.two.green = tDraw.currGreen;
+		tDraw.two.blue = tDraw.currBlue;
+
+		//Make sure our mins and maxes are current with this new vertex
+		if (tDraw.minX > tDraw.two.myX)
+			tDraw.minX = tDraw.two.myX;
+		if (tDraw.maxX < tDraw.two.myX)
+			tDraw.maxX = tDraw.two.myX;
+		if (tDraw.minY > tDraw.two.myY)
+			tDraw.minY = tDraw.two.myY;
+		if (tDraw.maxY < tDraw.two.myY)
+			tDraw.maxY = tDraw.two.myY;
+
+	}
+	//If we've initialized all of the other vertexes...do this one
+	else if (tDraw.three.initialized == 0)
+	{
+		tDraw.three.initialized = 1;
+		tDraw.three.myX = x;
+		tDraw.three.myY = y;
+		tDraw.three.red = tDraw.currRed;
+		tDraw.three.green = tDraw.currGreen;
+		tDraw.three.blue = tDraw.currBlue;
+
+		//Make sure our mins and maxes are current with this new vertex
+		if (tDraw.minX > tDraw.three.myX)
+			tDraw.minX = tDraw.three.myX;
+		if (tDraw.maxX < tDraw.three.myX)
+			tDraw.maxX = tDraw.three.myX;
+		if (tDraw.minY > tDraw.three.myY)
+			tDraw.minY = tDraw.three.myY;
+		if (tDraw.maxY < tDraw.three.myY)
+			tDraw.maxY = tDraw.three.myY;
+
+
+	}
+
+}
+//All vertices of the polygon have been given so draw it and write them to the framebuffer. 
+void osuEnd()
+{
+	printf("Drawing is over?\n");
+	//We're drawing lines now, not triangles.
+
+	/*Vec3 myN, myNQ, myNR, myNS;
+	int myRed, myGreen, myBlue;
+	float p1, p2, p3;
+	float aC, bC, cC, pC;
+	float a, b, c, p;
+	float area, areaOne, areaTwo, areaThree;
+
+	//Vectors of our triangle sides
+	Vec3 one = Vec3(tDraw.one.myX, tDraw.one.myY, 0.);
+	Vec3 two = Vec3(tDraw.two.myX, tDraw.two.myY, 0.);
+	Vec3 three = Vec3(tDraw.three.myX, tDraw.three.myY, 0.);
+
+	//Computing the Area of the entire triangle that we're drawing. 
+	aC = (two - three).Length();
+	bC = (one - two).Length();
+	cC = (three - one).Length();
+	pC = (aC + bC + cC) / 2.;
+	area = sqrt(pC*(pC - aC)*(pC - bC)*(pC - cC));
+
+	Vec3 currPos = Vec3(0., 0., 0.);
+	//Loop through this triangles minimum/maximum width
+	for (int i = (tDraw.minX); i < tDraw.maxX + 1; i++)
+	{
+		//Loop through it's minumum/maximum heigth.b
+		for (int j = (tDraw.minY); j < (tDraw.maxY + 1); j++)
+		{
+			//Three dimensional point inside a triangle implmentation:
+			//http://web.engr.oregonstate.edu/~mjb/cs491/Handouts/vectors.1pp.pdf
+			//Initialize our point within this space
+			currPos = Vec3(i, j, 0.);
+			//Get the Normal vector of the triangle
+			myN = (three - two).Cross(one - two);
+			myNQ = (three - two).Cross(currPos - two);
+			myNR = (one - three).Cross(currPos - three);
+			myNS = (two - one).Cross(currPos - one);
+			//do my cross products conclude that the points inside my triangle?			
+			if ((myN.Dot(myNQ) > 0) && (myN.Dot(myNR) >= 0) && (myN.Dot(myNS) > 0))
+			{
+				//Interpolation--------------------------------	
+				//First subtriangle area....
+				a = (currPos - three).Length();
+				b = (one - currPos).Length();
+				c = (three - one).Length();
+				p = (a + b + c) / 2.;
+				areaOne = sqrt(p*(p - a)*(p - b)*(p - c));
+				//Second subtriangle area.....
+				a = (currPos - two).Length();
+				b = (three - currPos).Length();
+				c = ((two - three).Length());
+				p = (a + b + c) / 2.;
+				areaTwo = sqrt((p*(p - a)*(p - b)*(p - c)));
+				//Third subtriangle area.....
+				a = (currPos - two).Length();
+				b = (one - currPos).Length();
+				c = ((two - one).Length());
+				p = (a + b + c) / 2.;
+				areaThree = sqrt(p*(p - a)*(p - b)*(p - c));
+				//Now Convert our areas to a proportional amount of the triangle
+				p1 = areaTwo / area;
+				p2 = areaOne / area;
+				p3 = areaThree / area;
+				//Apply these proportions to the individual pixel color..
+				myRed = ((p1 * tDraw.one.red) + (p2 * tDraw.two.red) + (p3 * tDraw.three.red));
+				myGreen = ((p1 * tDraw.one.green) + (p2 * tDraw.two.green) + (p3 * tDraw.three.green));
+				myBlue = ((p1 * tDraw.one.blue) + (p2 * tDraw.two.blue) + (p3 * tDraw.three.blue));
+				// End of interpolation ---------------------------------------------------------------
+				//Write to our beloved pixel. 
+				osuWritePixel(i, j, myRed, myGreen, myBlue);
+			}
+		}
+	}*/
+	
+}
+
+
+
+void osuVertex3f(double x, double y, double z)
+{
+	if (lDraw.one.intialized == 0)
+	{
+		lDraw.one.intialized = 1;
+		lDraw.one.pos = Vec3((float)x,(float)y,(float)z);
+	}
+	else if (lDraw.two.intialized == 0)
+	{
+		int w, h;
+		osuGetFramebufferSize(&w, &h);
+		lDraw.two.intialized = 1;
+		lDraw.two.pos = Vec3((float)x,(float)y,(float)z);
+		Vec3 p1, p2;
+		float x0, y0, x1, y1;
+		//If we need to perform a orthographic projection
+		if ((ORTHO == TRUE) && (PROJECTION == FALSE))
+		{
+			p1 =(orthoProj *((Current_Transform * lDraw.one.pos)));
+			p2 =(orthoProj *((Current_Transform * lDraw.two.pos)));
+			x0 = p1.x; y0 = p1.y; x1 = p2.x; y1 = p2.y;
+			printf("O:Tried to draw a line %f %f %f %f...\n",x0*w,y0*h,x1*w,y1*h);
+
+			draw_line(x0*w, y0*h, x1*w, y1*h);
+		}
+		//We need to perform a perspective projection
+		else if ((PROJECTION == TRUE) && (ORTHO == FALSE))
+		{
+			p1 = persProj * ((Current_Transform) * lDraw.one.pos);
+			p2 = persProj * ((Current_Transform) * lDraw.two.pos);
+			x0 = p1.x; y0 = p1.y; x1 = p2.x; y1 = p2.y;
+			printf("P:Tried to draw a line %f-%f %f-%f...\n",x0*w,y0*h,x1*w,y1*h);
+
+			draw_line(x0*w, y0*h, x1*w, y1*h);
+		}
+		//Something got messed up.
+		else
+			printf("Neither a project or Orthographic was given, or BOTH we're given concurrently.\n");
+		lDraw = {};
+	}
+	
+}
+
+void osuInitialize() 
+{ 
+	//Create an identity matrix and push it on the stack
+	Mat4 transformMat = Mat4();
+	Current_Transform = transformMat;
+	printf("Pushing identity Matrix onto the matStack.\n");
+	matStack.push(transformMat);
+
+}
+
+
+void osuPushMatrix() 
+{ 
+	Mat4 newTop = Mat4(matStack.front());
+	Current_Transform = newTop;
+	matStack.push(newTop);
+}
+
+void osuPopMatrix() 
+{ 
+	if (matStack.size() > 1)
+	{   //Ok you can do it.
+		matStack.pop();
+		Current_Transform = matStack.front();
+	}
+	else
+	{
+		printf("Pop: Can't perform action on Identity Matrix.\n");
+	}
+}
+
+void osuLoadIdentityMatrix()
+{
+	//Set the current Transform to the identity matrix
+	Current_Transform.SetIdentity();
+}
+void osuTranslate(double tx, double ty, double tz) 
+{ 
+	Current_Transform.SetTranslate(tx, ty, tz);
+}
+
+void osuScale(double sx, double sy, double sz) 
+{ 
+	Current_Transform.SetScale(sx, sy, sz);
+}
+
+void osuRotate(double angle, double ax, double ay, double az) { 
+	
+	Current_Transform.SetRotateX(angle);
+	Current_Transform.SetRotateY(angle);
+	Current_Transform.SetRotateZ(angle);
+		
+}
+
+void osuLookat(double from[3], double at[3], double up[3])
+{
+	//From = vector specifying direction from which the viewer would like to view the scene.
+	//myView.SetTranslate(-from[0], -from[1], -from[2]);
+	Vec3 fVec, aVec, uVec,e,v, w;
+	
+	fVec = Vec3(from[0], from[1], from[2]);
+	uVec = Vec3(up[0], up[1], up[2]);
+	aVec = Vec3(at[0], at[1], at[2]);
+	e = fVec;
+	v = uVec - fVec;
+	w = fVec - aVec;
+	
+	v = v.Unit();
+	w = w.Unit();
+	u = v.Cross(w);
+
+
+	myView.SetCamera(u);
+
+}
+
+void osuBegin(OSUDrawable mode)
+{
+	if (mode == OSU_TRIANGLE)
+	{
+		//We're going to draw a triangle, so intialize our data structure to hold it
+		tDraw = {};
+		//Also make sure to set a default color, incase the user forgets to give you one. 
+		osuColor3f(1.0, 1.0, 1.0);
+
+	}
+	else if (mode == OSU_LINES)
+	{
+		printf("Time for vertexes\n");
+
+		lDraw = {};
+		double f[3] = {0.0,0.0,1.0}, a[3] = {0.,0.,1. }, myUp[3] = { 0.,0.,1. };
+		osuLookat(f, a, myUp);
+	}
+}
